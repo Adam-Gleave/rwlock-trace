4b2b7bd7aed972e121c9c4d472164325ebf13882
Adam-Gleave <adamg108@hotmail.co.uk>
2021-04-19 14:58:17 +0100

    Add unique identifiers to RwLock instances, move acquire message, add got message

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,61 @@
-use std::ops::{Deref, DerefMut};
+use std::{ops::{Deref, DerefMut}, sync::atomic::{AtomicU64, Ordering}};
 
 use backtrace::{Backtrace, BacktraceFrame};
+use names::Generator;
 
 const FRAME_OFFSET: usize = 3;
 
 #[derive(Debug)]
-pub struct RwLock<T>(tokio::sync::RwLock<T>);
+pub struct RwLock<T> {
+    lock: tokio::sync::RwLock<T>,
+    name: String,
+    idx: AtomicU64,
+}
 
 impl<'a, T> RwLock<T> {
     pub fn new(inner: T) -> Self {
-        Self(tokio::sync::RwLock::new(inner))
+        let mut generator = Generator::default();
+
+        Self {
+            lock: tokio::sync::RwLock::new(inner),
+            name: generator.next().unwrap(),
+            idx: AtomicU64::new(0),
+        }
     }
 
     pub async fn read(&'a self) -> RwLockReadGuard<'a, T> {
-        RwLockReadGuard::new(self.0.read().await)
+        self.idx.fetch_add(1, Ordering::SeqCst);
+        let idx = self.idx.load(Ordering::SeqCst);
+        log_backtrace(&format!("[READ] Acquire ({}:{})", self.name, idx));
+        RwLockReadGuard::new(self.lock.read().await, &self.name, idx)
     }
 
     pub async fn write(&'a self) -> RwLockWriteGuard<'a, T> {
-        RwLockWriteGuard::new(self.0.write().await)
+        self.idx.fetch_add(1, Ordering::SeqCst);
+        let idx = self.idx.load(Ordering::SeqCst);
+        log_backtrace(&format!("[WRITE] Acquire ({}:{})", self.name, idx));
+        RwLockWriteGuard::new(self.lock.write().await, &self.name, idx)
     }
 }
 
 #[derive(Debug)]
-pub struct RwLockReadGuard<'a, T>(tokio::sync::RwLockReadGuard<'a, T>);
+pub struct RwLockReadGuard<'a, T> {
+    guard: tokio::sync::RwLockReadGuard<'a, T>,
+    name: &'a str,
+    idx: u64,
+}
 
 impl<'a, T> RwLockReadGuard<'a, T> {
-    pub fn new(inner: tokio::sync::RwLockReadGuard<'a, T>) -> Self {
-        log_backtrace("[READ] Acquire");
-        Self(inner)
+    pub fn new(inner: tokio::sync::RwLockReadGuard<'a, T>, name: &'a str, idx: u64) -> Self {
+        let new = Self { guard: inner, name, idx };
+        log_backtrace(&format!("[READ] Got ({}:{})", name, idx));
+        new
     }
 }
 
 impl<'a, T> Drop for RwLockReadGuard<'a, T> {
     fn drop(&mut self) {
-        log_backtrace("[READ] Release");
+        log_backtrace(&format!("[READ] Release ({}:{})", self.name, self.idx));
     }
 }
 
@@ -41,29 +63,34 @@ impl<'a, T> Deref for RwLockReadGuard<'a, T> {
     type Target = tokio::sync::RwLockReadGuard<'a, T>;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.guard
     }
 }
 
 impl<'a, T> DerefMut for RwLockReadGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut tokio::sync::RwLockReadGuard<'a, T> {
-        &mut self.0
+        &mut self.guard
     }
 }
 
 #[derive(Debug)]
-pub struct RwLockWriteGuard<'a, T>(tokio::sync::RwLockWriteGuard<'a, T>);
+pub struct RwLockWriteGuard<'a, T> {
+    guard: tokio::sync::RwLockWriteGuard<'a, T>,
+    name: &'a str,
+    idx: u64,
+}
 
 impl<'a, T> RwLockWriteGuard<'a, T> {
-    pub fn new(inner: tokio::sync::RwLockWriteGuard<'a, T>) -> Self {
-        log_backtrace("[WRITE] Acquire");
-        Self(inner)
+    pub fn new(inner: tokio::sync::RwLockWriteGuard<'a, T>, name: &'a str, idx: u64) -> Self {
+        let new = Self { guard: inner, name, idx };
+        log_backtrace(&format!("[WRITE] Got ({}:{})", name, idx));
+        new
     }
 }
 
 impl<'a, T> Drop for RwLockWriteGuard<'a, T> {
     fn drop(&mut self) {
-        log_backtrace("[WRITE] Release");
+        log_backtrace(&format!("[WRITE] Release ({}:{})", self.name, self.idx));
     }
 }
 
@@ -71,13 +98,13 @@ impl<'a, T> Deref for RwLockWriteGuard<'a, T> {
     type Target = tokio::sync::RwLockWriteGuard<'a, T>;
 
     fn deref(&self) -> &Self::Target {
-        &self.0
+        &self.guard
     }
 }
 
 impl<'a, T> DerefMut for RwLockWriteGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut tokio::sync::RwLockWriteGuard<'a, T> {
-        &mut self.0
+        &mut self.guard
     }
 }
 
@@ -107,10 +134,11 @@ fn log_backtrace(message: &str) {
 
     let output = symbols.fold(String::new(), |acc, (i, s)| {
         acc + format!(
-            "[{}] {}:{}\n",
+            "[{}] {}:{} {}\n",
             i - FRAME_OFFSET,
             s.filename().unwrap().to_string_lossy(),
             s.lineno().unwrap(),
+            s.name().unwrap(),
         )
         .as_str()
     });
